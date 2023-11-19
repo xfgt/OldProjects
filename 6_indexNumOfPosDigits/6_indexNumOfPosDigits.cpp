5ee50cc759e5202fa0d4986f0272b5c4ee97f658
gitGUzz <waffle_man562@yahoo.com>
2023-11-19 13:07:22 +0200

    solution

--- 6_indexNumOfPosDigits/6_indexNumOfPosDigits.cpp
+++ 6_indexNumOfPosDigits/6_indexNumOfPosDigits.cpp
@@ -6,9 +6,17 @@ int main()
     int n = 0;
     int num = 0;
 
-    bool firstPositive = false; 
-    bool serialPositive = false; 
-    bool lastPositive = false; 
+    bool boolFirstPositive = false; 
+    bool boolSerialPositive = false; 
+    bool boolLastPositive = false; 
+
+
+
+    int numFirstPositive = 0;
+    int numFirstPositivePosition = 0;
+
+    int numSerialPositive = 0;
+    int numSerialPositivePosition = 0;
 
     int numLastPositive = 0;
     int numLastPositivePosition = 0;
@@ -40,29 +48,32 @@ int main()
         std::cout << std::endl;
         for (int i = 0; i < n; i++)
         {
+            
             if (analyzeArr[i] > 0) {
-                
-                if (firstPositive && serialPositive) {
-                    firstPositive = true;
+                if (boolFirstPositive) {
+                    numSerialPositive = analyzeArr[i];
+                    numSerialPositivePosition = i;
+
                     numLastPositive = analyzeArr[i];
                     numLastPositivePosition = i;
-                    std::cout << "\nSERIAL POSITIVE HIT!: " << analyzeArr[i] << "\nPOSITION: " << i << std::endl;
-                }
-                else if (firstPositive = true) {
-                    serialPositive = true;
-                    std::cout << "\nFIRST POSITIVE HIT!: " << analyzeArr[i] << "\nPOSITION: " << i << std::endl;
 
+                    std::cout << "serialPositive: " << numSerialPositive << " on position: " << numSerialPositivePosition << std::endl;
+                }
+                else {
+                    numFirstPositive = analyzeArr[i];
+                    numFirstPositivePosition = i;
+
+                    numLastPositive = analyzeArr[i];
+                    numLastPositivePosition = i;
+
+                    std::cout << "firstPositive: " << numFirstPositive << " on position: " << numFirstPositivePosition << std::endl;
+                    boolFirstPositive = true;
                 }
 
-                if (n - i == 1) {
-                    std::cout << "\nTHE LAST POSITIVE HIT!: " << numLastPositive << "\nPOSITION: " << numLastPositivePosition << std::endl;
-                }
-                
-                
-                
-               
-
-                
+            }
+            if (n - i == 1) {
+                std::cout << "lastPositive: " << numLastPositive << " on position: " << numLastPositivePosition << std::endl;
+                break;
             }
         }
 
