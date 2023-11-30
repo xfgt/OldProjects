53afa3c4852c0a2b2bbbedd8c9d46f0f21d7d74f
gitGUzz <waffle_man562@yahoo.com>
2023-11-30 13:55:00 +0200

    comment lcm

--- 3_LCM/3_LCM.cpp
+++ 3_LCM/3_LCM.cpp
@@ -43,9 +43,7 @@ int main()
         std::cout << "XZ = "<< GCD_XZ << std::endl;*/
 
 
-        // search for the greatest
-
-
+        // search for the lowest <<<
         if (GCD_XY < GCD_YZ) {
             if (GCD_XY < GCD_XZ) {
                 GCD = GCD_XY;
@@ -74,13 +72,51 @@ int main()
             GCD = GCD_XY;
         }
 
-        LCM_XY = (GCD_XY * GCD_YZ) / GCD;
-        LCM_YZ = (GCD_YZ * GCD_XZ) / GCD;
-        LCM_XZ = (GCD_XY * GCD_XZ) / GCD;
-        std::cout << "GCD = " << GCD << std::endl;
-        std::cout << "LCM_XY = " << LCM_XY << std::endl;
-        std::cout << "LCM_YZ = " << LCM_YZ << std::endl;
-        std::cout << "LCM_XZ = " << LCM_XZ << std::endl;
+        /*  15, 16, 17
+        * 
+        * 
+            XY = (15 * 16) / 1(GCD) = 240
+            YZ = (16 * 17) / 1(GCD) = 272
+            XZ = (15 * 17) / 1(GCD)  = 255
+                                    find largest of results = answer    
+
+                                    LCM = 4080 / 272 = 15
+        */
+        LCM_XY = (x * y) / GCD;
+        LCM_YZ = (y * z) / GCD;
+        LCM_XZ = (x * z) / GCD;
+
+        //search for the greatest >>>
+        if (LCM_XY > LCM_YZ) {
+            if (LCM_XY > LCM_XZ) {
+                LCM = LCM_XY;
+            }
+            else {
+                LCM = LCM_XZ;
+            }
+        }
+        else if (LCM_YZ > LCM_XY) {
+            if (LCM_YZ > LCM_XZ) {
+                LCM = LCM_YZ;
+            }
+            else {
+                LCM = LCM_XZ;
+            }
+        }
+        else if (LCM_XZ > LCM_XY) {
+            if (LCM_XZ > LCM_YZ) {
+                LCM = LCM_XZ;
+            }
+            else {
+                LCM = LCM_YZ;
+            }
+        }
+        else {
+            LCM = LCM_XY;
+        }
+
+        std::cout << LCM << std::endl;
+
 
     }
     else {
