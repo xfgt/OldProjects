32ae5702a5d1a2eada9d0012cad9733aa183ec12
gitGUzz <waffle_man562@yahoo.com>
2023-11-30 13:39:05 +0200

    gcd done, lcm half working

--- 3_LCM/3_LCM.cpp
+++ 3_LCM/3_LCM.cpp
@@ -1,5 +1,89 @@
+
+
 #include <iostream>
 
-int main() {
 
+
+int main()
+{
+    //x, y ,z
+    //xy, yz, xz
+
+    int x, y, z, GCD_XY, GCD_YZ, GCD_XZ, GCD, LCM_XY, LCM_YZ, LCM_XZ, LCM;
+    std::cin >> x >> y >> z;
+    if (x >= 10 && x <= 100 && y >= 10 && y <= 100 && z >= 10 && z <= 100) {
+
+
+        // XY
+        for (int i = 1; i <= x && i <= y; i++)
+        {
+            if (x % i == 0 && y % i == 0) {
+                GCD_XY = i;
+            }
+        }
+
+        // YZ
+        for (int i = 1; i <= y && i <= z; i++)
+        {
+            if (y % i == 0 && z % i == 0) {
+                GCD_YZ = i;
+            }
+        }
+        // XZ
+        for (int i = 1; i <= x && i <= z; i++)
+        {
+            if (x % i == 0 && z % i == 0) {
+                GCD_XZ = i;
+            }
+        }
+
+
+        /*std::cout << "XY = "<< GCD_XY << std::endl;
+        std::cout << "YZ = "<< GCD_YZ << std::endl;
+        std::cout << "XZ = "<< GCD_XZ << std::endl;*/
+
+
+        // search for the greatest
+
+
+        if (GCD_XY < GCD_YZ) {
+            if (GCD_XY < GCD_XZ) {
+                GCD = GCD_XY;
+            }
+            else {
+                GCD = GCD_XZ;
+            }
+        }
+        else if (GCD_YZ < GCD_XY) {
+            if (GCD_YZ < GCD_XZ) {
+                GCD = GCD_YZ;
+            }
+            else {
+                GCD = GCD_XZ;
+            }
+        }
+        else if (GCD_XZ < GCD_XY) {
+            if (GCD_XZ < GCD_YZ) {
+                GCD = GCD_XZ;
+            }
+            else {
+                GCD = GCD_YZ;
+            }
+        }
+        else {
+            GCD = GCD_XY;
+        }
+
+        LCM_XY = (GCD_XY * GCD_YZ) / GCD;
+        LCM_YZ = (GCD_YZ * GCD_XZ) / GCD;
+        LCM_XZ = (GCD_XY * GCD_XZ) / GCD;
+        std::cout << "GCD = " << GCD << std::endl;
+        std::cout << "LCM_XY = " << LCM_XY << std::endl;
+        std::cout << "LCM_YZ = " << LCM_YZ << std::endl;
+        std::cout << "LCM_XZ = " << LCM_XZ << std::endl;
+
+    }
+    else {
+        std::cout << "Invalid input data!" << std::endl;
+    }
 }
--- GDC/2_GCD.cpp
+++ GDC/2_GCD.cpp
@@ -6,82 +6,75 @@
 
 int main()
 {
-    int x, y, z;
-    std::cin >> x >> y >> z;
+    //x, y ,z
+    //xy, yz, xz
 
+    int x, y, z, GCD_XY, GCD_YZ, GCD_XZ, GCD;
+    std::cin >> x >> y >> z;
     if (x >= 20 && x <= 300 && y >= 20 && y <= 300 && z >= 20 && z <= 300) {
-        int smallestN = 0;
-        if (x < y) {
-            if (x < z) {
-                smallestN = x;
-            }
-            else {
-                smallestN = z;
-            }
-        }
-        else if (y < x) {
-            if (y < z) {
-                smallestN = y;
-            }
-            else {
-                smallestN = z;
+        
+       
+        // XY
+        for (int i = 1; i <= x && i <= y; i++)
+        {
+            if (x % i == 0 && y % i == 0) {
+                GCD_XY = i;
             }
         }
-        else if (z < x) {
-            if (z < y) {
-                smallestN = z;
+
+        // YZ
+        for (int i = 1; i <= y && i <= z; i++)
+        {
+            if (y % i == 0 && z % i == 0) {
+                GCD_YZ = i;
+            }
+        }
+        // XZ
+        for (int i = 1; i <= x && i <= z; i++)
+        {
+            if (x % i == 0 && z % i == 0) {
+                GCD_XZ = i;
+            }
+        }
+        
+
+        /*std::cout << "XY = "<< GCD_XY << std::endl;
+        std::cout << "YZ = "<< GCD_YZ << std::endl;
+        std::cout << "XZ = "<< GCD_XZ << std::endl;*/
+
+
+        // search for the greatest
+
+       
+        if (GCD_XY < GCD_YZ) {
+            if (GCD_XY < GCD_XZ) {
+                GCD = GCD_XY;
             }
             else {
-                smallestN = y;
+                GCD = GCD_XZ;
+            }
+        }
+        else if (GCD_YZ < GCD_XY) {
+            if (GCD_YZ < GCD_XZ) {
+                GCD = GCD_YZ;
+            }
+            else {
+                GCD = GCD_XZ;
+            }
+        }
+        else if (GCD_XZ < GCD_XY) {
+            if (GCD_XZ < GCD_YZ) {
+                GCD = GCD_XZ;
+            }
+            else {
+                GCD = GCD_YZ;
             }
         }
         else {
-            smallestN = x;
+            GCD = GCD_XY;
         }
-        //std::cout << "smallest num: " << smallestN << '\n';
+        std::cout << GCD;
 
-
-        int gcd = 0;
-        bool foundGCD = false;
-
-
-        while (!foundGCD) {
-            for (int i = smallestN; i >= 1; i--)
-            {
-
-                if (x / i != 1 && y / i != 1 && z / i != 1) {
-
-                    if (x % i == 0) {
-                        if (y % i == 0) {
-                            if (z % i == 0) {
-                                gcd = i;
-                                foundGCD = true;
-                                break;
-                            }
-                        }
-                    }
-
-
-                }
-                else {
-                    if (x == y == z || x == y || x == z || y == z) {
-                        gcd = smallestN;
-                        foundGCD = true;
-                        break;
-
-                    }
-                    else {
-                        gcd = i;
-                        foundGCD = true;
-                    }
-                   
-                    
-                    
-                }
-            }
-        }
-
-        std::cout << gcd << std::endl;
     }
     else {
         std::cout << "Invalid input data!" << std::endl;
