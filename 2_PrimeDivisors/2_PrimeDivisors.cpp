c08e7ed6a81111cb7e47a7c86c756db05dda5d70
gitGUzz <waffle_man562@yahoo.com>
2023-12-02 23:51:47 +0200

    solution 2_PrimeDivisors

--- 2_PrimeDivisors/2_PrimeDivisors.cpp
+++ 2_PrimeDivisors/2_PrimeDivisors.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
+bool isPrime(int n)
+{
+    if (n == 1 || n == 0) return false;
 
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0) return false;
+    }
+    
+    return true;
+}
 int main()
 {
-    // if(i == 2 || i % 2 == 1|| i % 3 == 0 && i % 3 == 2) --> odd nums
-
     int n;
     std::cin >> n;
     
     int result = 0;
     
-
     if (n >= 2 && n <= 2000) {
-        for (int i = 2; i <= n/2; ++i)
+        for (int i = 1; i <= n; i++)
         {
-            if(n % i == 0)
-                if (i == 2 || i % 2 == 1 ) {
-                    if (i % 3 == 0 || i % 3 == 2) {
-                        std::cout << i << " ";
-                        result += i;
-                    }
-                }
+            if (isPrime(i))
+                if (n % i == 0)
+                    result += i;
             
         }
-        std::cout << std::endl << "result: " << result << std::endl;
+
+
+        std::cout << result << std::endl;
+
     }
     else {
         std::cout << "Invalid input data!" << std::endl;
