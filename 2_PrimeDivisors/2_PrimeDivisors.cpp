2b439515af62bcebb80ab4a6f6a60101cfbf4de1
gitGUzz <waffle_man562@yahoo.com>
2023-12-02 14:23:30 +0200

    checkpoitnt2

--- 2_PrimeDivisors/2_PrimeDivisors.cpp
+++ 2_PrimeDivisors/2_PrimeDivisors.cpp
@@ -13,9 +13,11 @@ int main()
     if (n >= 2 && n <= 2000) {
         for (int i = 2; i <= n/2; ++i)
         {
-            if(i == 2 || i % 2 == 1|| i % 3 == 0 && i % 3 == 2)
-                /*if(n % i == 0)*/
+            if(n % i == 0)
+                if (i == 2 || i % 2 == 1 || i % 3 == 0 && i % 3 == 2) {
                     std::cout << i << " ";
+                    result += i;
+                }
             
         }
         std::cout << std::endl << "result: " << result << std::endl;
