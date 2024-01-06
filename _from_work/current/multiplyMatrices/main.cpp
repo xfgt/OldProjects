c5dd7279aaed51bccf82ed063accd3f10e82e252
xfgt <waffle_man562@yahoo.com>
2024-01-06 20:31:21 +0200

    diagonal values

---
+++ _from_work/current/multiplyMatrices/main.cpp
@@ -0,0 +1,98 @@
+
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    int nA = 0;
+    int mA = 0;
+
+    int nB = 0;
+    int mB = 0;
+
+    int num = 0;
+
+    std::cout << "== A matrix size ==" << std::endl;
+    std::cout << "(A)N = "; std::cin >> nA;
+    std::cout << "(A)M = "; std::cin >> mA;
+
+    std::cout << "== B matrix size ==" << std::endl;
+    std::cout << "(B)N = "; std::cin >> nB;
+    std::cout << "(B)M = "; std::cin >> mB;
+
+
+
+    if(nA >= 1 && nA <= 30 && nB >= 1 && nB <= 30){
+        if(mA >= 1 && mA <= 40 && mB >= 1 && mB <= 40){
+
+            int ** A = new int*[nA];
+            for(int i = 0; i < nA; i++){
+                A[i] = new int[mA];
+            }
+
+            int ** B = new int*[nB];
+            for(int i = 0; i < nB; i++){
+                B[i] = new int[mB];
+            }
+
+            if(mA == nB){
+
+                int ** C = new int*[nA];
+
+                for(int i = 0; i < nA; i++){
+                    C[i] = new int[nA];
+                }
+
+                std::cout << "values for A matrix: " << std::endl;
+                for(int i = 0; i < nA; i++){
+                    for(int j = 0; j < mA; j++){
+                        std::cin >> num;
+                        A[i][j] = num;
+                    }
+                }
+                num = 0;
+                std::cout << "values for B matrix: " << std::endl;
+                for(int i = 0; i < nB; i++){
+                    for(int j = 0; j < mB; j++){
+                        std::cin >> num;
+                        B[i][j] = num;
+                    }
+                }
+                int sum = 0;
+
+                int i = 0;
+                int j = 0;
+
+                for(i = 0; i <= nB; i++){
+                    for(j = 0; j <= mB; j++){
+                        sum += A[i][j] * B[j][i];
+                    }
+                    std::cout << sum << std::endl;
+                    sum = 0;
+                }
+
+
+               //TODO C array
+
+                // std::cout << std::endl << "C matrix: " << std::endl;
+
+
+                /** MEMORY MANAGEMENT **/
+
+                for(int i = 0; i < mA; i++){
+                    delete[] A[i];
+                    delete[] B[i];
+                    delete[] C[i];
+                }
+                delete[] A;
+                delete[] B;
+                delete[] C;
+                A = NULL;
+                B = NULL;
+                C = NULL;
+            }
+        }
+    }
+
+    return 0;
+}
