4a28492a03ee6b32e6f7f1a26153e4f06afa136a
xfgt <waffle_man562@yahoo.com>
2024-02-09 11:58:51 +0200

    cscb035 homework 1-4

--- 1_BezoutsTheorem/1_BezoutsTheorem.cpp
+++ 1_BezoutsTheorem/1_BezoutsTheorem.cpp
@@ -1,46 +1,147 @@
 #include <iostream>
+#include <cmath>
+
 
 int gcd(int a, int b)
 {
-	
-	int result = std::min(a, b);
-	while (result > 0) {
-		if (a % result == 0 && b % result == 0) {
-			break;
-		}
-		result--;
-	}
-	
-	return result;
+	if (a == 0)
+		return b;
+	return gcd(b % a, a);
 }
 
 
-int main()
-{
+
+int BezoutsCoefs(int a, int b, int pos) { //pos [0, 1] (s,t)
+	int const ROWS = 4;
 	
-	// solve:		a*x+b*y=0 (mod c)
+
+	int cnt = 0;
+	int x = (a > b) ? a : b;
+	int y = (a < b) ? a : b;
 	
-	// x = s * (c / (a, b))
-	// y = t * (c / (a, b))
+	int tmp = x % y;
+	while (tmp) {
+		x = (tmp > y) ? tmp : y;
+		y = (tmp < y) ? tmp : y;
+		tmp = x % y;
+		cnt++;
+	}
+	
+
+	if (cnt > 0) {
+		int** arr = new int* [ROWS];
+		for ( int i = 0; i < ROWS; i++) {
+			arr[i] = new int[cnt + 2]; // s & t
+		}
+		x = (a > b) ? a : b;
+		y = (a < b) ? a : b;
+		arr[0][0] = x;
+		arr[0][1] = y;
+
+		arr[1][0] = 0;
+
+		arr[2][0] = 1;
+		arr[2][1] = 0;
+
+		arr[3][0] = 0;
+		arr[3][1] = 1;
+
+		int j = 2;
+		tmp = x % y;
+		while (tmp) {
+			arr[0][j] = tmp;		// mods 
+			arr[1][j - 1] = x / y;	// divs
+			x = (tmp > y) ? tmp : y;
+			y = (tmp < y) ? tmp : y;
+			tmp = x % y;
+			j++;
+		}
+		arr[1][j - 1] = x / y; // endpoint
+
+
+		for ( int i = 2; i < ROWS; i++) {
+			for ( int j = 2; j < cnt + 2; j++) {
+				if (i == 2)
+					arr[i][j] = arr[i - 1][j - 1] * arr[i][j - 1] + arr[i][j - 2];
+				else
+					arr[i][j] = arr[i - 2][j - 1] * arr[i][j - 1] + arr[i][j - 2];
+			}
+		}
+
+		int m = 0, s=0, t=0;
+		
+		for ( int j = 0; j < cnt + 2; j++) {
+			m = arr[0][j];
+			s = pow(-1, j) * arr[2][j];
+			t = pow(-1, j + 1) * arr[3][j];
+		}
+
+		/*std::cout << s << "\n" << t << "\n";*/
+		for ( int i = 0; i < ROWS; i++) {
+			delete[] arr[i];
+		}
+		delete[] arr;
+		if (pos == 0)
+			return s;
+		if (pos == 1)
+			return t;
+	}
+	else {
+		if (pos == 0)
+			return 0;
+		if (pos == 1)
+			return 1;
+	}
+
+	return 0;
+}
+
+
+
+
+int main() {
+	
 	int a, b, c;
 	std::cin >> a >> b >> c;
 
+	int GCD = gcd(a, b);
+	int division = c / GCD;
+	int s = BezoutsCoefs(a, b, 0);
+	int t = BezoutsCoefs(a, b, 1);
+	int x = 0;
+	int y = 0;
+	if (division != 0) {
+		x = s * division;
+		y = t * division;
+	}
+	int larger = 0;
+	int res = (a * x) + (b * y);
+		
+		/*std::cout << "[x = " << x << "]" << std::endl;
+		std::cout << "[y = " << y << "]" << std::endl;
+		std::cout << "[s = " << s << "]" << std::endl;
+		std::cout << "[t = " << t << "]" << std::endl;
+		std::cout << "[division = " << division << "]" << std::endl;
+		std::cout << "[GCD(a,b) = " << GCD << "]" << std::endl;
+		std::cout << "[res = " << res << "]" << std::endl;*/
+	
+
+
 	if (a >= 1 && a <= 100 && b >= 1 && b <= 100 && c >= 1 && c <= 100) {
-		if (gcd(a, b) % c == 0) {
+		if (GCD% c == 0 || division != 0) {
 
+			std::cout << x << std::endl;
+			std::cout << y << std::endl;
 			
-
-
-
-
-
-
+		}
+		else {
+			std::cout << "No solution!" << std::endl;
 		}
 	}
 	else {
-		std::cout << "Invalid input data!";
-		
+		std::cout << "Invalid input data!" << std::endl;
 		
 	}
+	return 0;
 }
 
--- 3_LCM/3_LCM.cpp
+++ 3_LCM/3_LCM.cpp
@@ -1,125 +1,31 @@
-
-
 #include <iostream>
 
 
+int gcd(int a, int b){ // recursion
+	if (a == 0)
+		return b;
+	return gcd(b % a, a);
+}
 
-int main()
-{
-    //x, y ,z
-    //xy, yz, xz
+int lcm(int a, int b) {
+	return (a * b) / gcd(a, b);
+}
 
-    int x, y, z, GCD_XY, GCD_YZ, GCD_XZ, GCD, LCM_XY, LCM_YZ, LCM_XZ, LCM;
-    std::cin >> x >> y >> z;
-    if (x >= 10 && x <= 100 && y >= 10 && y <= 100 && z >= 10 && z <= 100) {
+int main() {
 
+	int x, y, z;
 
-        // XY
-        for (int i = 1; i <= x && i <= y; i++)
-        {
-            if (x % i == 0 && y % i == 0) {
-                GCD_XY = i;
-            }
-        }
+	std::cin >> x >> y >> z;
 
-        // YZ
-        for (int i = 1; i <= y && i <= z; i++)
-        {
-            if (y % i == 0 && z % i == 0) {
-                GCD_YZ = i;
-            }
-        }
-        // XZ
-        for (int i = 1; i <= x && i <= z; i++)
-        {
-            if (x % i == 0 && z % i == 0) {
-                GCD_XZ = i;
-            }
-        }
-
-
-        /*std::cout << "XY = "<< GCD_XY << std::endl;
-        std::cout << "YZ = "<< GCD_YZ << std::endl;
-        std::cout << "XZ = "<< GCD_XZ << std::endl;*/
-
-
-        // search for the lowest <<<
-        if (GCD_XY < GCD_YZ) {
-            if (GCD_XY < GCD_XZ) {
-                GCD = GCD_XY;
-            }
-            else {
-                GCD = GCD_XZ;
-            }
-        }
-        else if (GCD_YZ < GCD_XY) {
-            if (GCD_YZ < GCD_XZ) {
-                GCD = GCD_YZ;
-            }
-            else {
-                GCD = GCD_XZ;
-            }
-        }
-        else if (GCD_XZ < GCD_XY) {
-            if (GCD_XZ < GCD_YZ) {
-                GCD = GCD_XZ;
-            }
-            else {
-                GCD = GCD_YZ;
-            }
-        }
-        else {
-            GCD = GCD_XY;
-        }
-
-        /*  15, 16, 17
-        * 
-        * 
-            XY = (15 * 16) / 1(GCD) = 240
-            YZ = (16 * 17) / 1(GCD) = 272
-            XZ = (15 * 17) / 1(GCD)  = 255
-                                    find largest of results = answer    
-
-                                    LCM = 4080 / 272 = 15
-        */
-        LCM_XY = (x * y) / GCD;
-        LCM_YZ = (y * z) / GCD;
-        LCM_XZ = (x * z) / GCD;
-
-        //search for the greatest >>>
-        if (LCM_XY > LCM_YZ) {
-            if (LCM_XY > LCM_XZ) {
-                LCM = LCM_XY;
-            }
-            else {
-                LCM = LCM_XZ;
-            }
-        }
-        else if (LCM_YZ > LCM_XY) {
-            if (LCM_YZ > LCM_XZ) {
-                LCM = LCM_YZ;
-            }
-            else {
-                LCM = LCM_XZ;
-            }
-        }
-        else if (LCM_XZ > LCM_XY) {
-            if (LCM_XZ > LCM_YZ) {
-                LCM = LCM_XZ;
-            }
-            else {
-                LCM = LCM_YZ;
-            }
-        }
-        else {
-            LCM = LCM_XY;
-        }
-
-        std::cout << LCM << std::endl;
-
-
-    }
-    else {
-        std::cout << "Invalid input data!" << std::endl;
-    }
+	if (x >= 10 && x <= 100) {
+		if (y >= 10 && y <= 100) {
+			if (z >= 10 && z <= 100) {
+				std::cout << lcm(lcm(x, y), lcm(y, z));
+				return 0;
+			}
+		}			
+	}
+	
+	std::cout << "Invalid input data!" << std::endl;
+	return 0;
 }
