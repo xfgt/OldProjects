2ce9f65496a02f74877cede31c77d30296fdc31c
xfgt <waffle_man562@yahoo.com>
2024-02-09 12:12:26 +0200

    Revert "cscb035 homework 1-4"
    This reverts commit 4a28492a03ee6b32e6f7f1a26153e4f06afa136a.

--- myFucntions/myFunctions.cpp
+++ myFucntions/myFunctions.cpp
@@ -45,10 +45,6 @@ int gcd(int a, int b)
 	return gcd(b % a, a);
 }
 
-int lcm(int a, int b) {
-	return (a * b) / gcd(a, b);
-}
-
 int findGCDArr(char * arr, int arrSize)
 {
 	int result = arr[0];
@@ -65,91 +61,7 @@ int findGCDArr(char * arr, int arrSize)
 }
 
 
-int BezoutsCoefs(int a, int b, int pos) { //pos [0, 1] (s,t)
-	int const ROWS = 4;
-	
-
-	int cnt = 0;
-	int x = (a > b) ? a : b;
-	int y = (a < b) ? a : b;
-	int tmp = x % y;
-	while (tmp) {
-		x = (tmp > y) ? tmp : y;
-		y = (tmp < y) ? tmp : y;
-		tmp = x % y;
-		cnt++;
-	}
-	
-
-	if (cnt > 0) {
-		int** arr = new int* [ROWS];
-		for (unsigned int i = 0; i < ROWS; i++) {
-			arr[i] = new int[cnt + 2]; // s & t
-		}
-		x = (a > b) ? a : b;
-		y = (a < b) ? a : b;
-		arr[0][0] = x;
-		arr[0][1] = y;
-
-		arr[1][0] = 0;
-
-		arr[2][0] = 1;
-		arr[2][1] = 0;
-
-		arr[3][0] = 0;
-		arr[3][1] = 1;
-
-		int j = 2;
-		tmp = x % y;
-		while (tmp) {
-			arr[0][j] = tmp;		// mods 
-			arr[1][j - 1] = x / y;	// divs
-			x = (tmp > y) ? tmp : y;
-			y = (tmp < y) ? tmp : y;
-			tmp = x % y;
-			j++;
-		}
-		arr[1][j - 1] = x / y; // endpoint
-
-
-		for (unsigned int i = 2; i < ROWS; i++) {
-			for (unsigned int j = 2; j < cnt + 2; j++) {
-				if (i == 2)
-					arr[i][j] = arr[i - 1][j - 1] * arr[i][j - 1] + arr[i][j - 2];
-				else
-					arr[i][j] = arr[i - 2][j - 1] * arr[i][j - 1] + arr[i][j - 2];
-			}
-		}
-
-
-		int m, s, t;
-		for (unsigned int j = 0; j < cnt + 2; j++) {
-			m = arr[0][j];
-			s = pow(-1, j) * arr[2][j];
-			t = pow(-1, j + 1) * arr[3][j];
-		}
-
-		/*std::cout << s << "\n" << t << "\n";*/
-		for (unsigned int i = 0; i < ROWS; i++) {
-			delete[] arr[i];
-		}
-		delete[] arr;
-		if (pos == 0)
-			return s;
-		if (pos == 1)
-			return t;
-	} 
-	else {
-		if (pos == 0)
-			return 0;
-		if (pos == 1)
-			return 1;
-	}
-
-return 0;
-}
-
-bool isPrime (int number){ // if number > 1 ?
+bool isPrime (int number){
     for(int i = 2; i < number; i++){
         if(number % i == 0){
             return false;
